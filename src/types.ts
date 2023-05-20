e6abcc5e2f483ff62bf538e5086d4d149fb03872
bragamat <mateuscbraga@gmail.com>
2023-05-19 21:07:16 -0300

    chore: fixes exporting types

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,12 @@
-type Splitted<Str extends string, Delimiter extends string> = string extends Str ? string[] :
-  Str extends '' ? [] :
-  Str extends `${infer Chunk}${Delimiter}${infer Rest}`
-  ? [Chunk, ...Splitted<Rest, Delimiter>]
-  : [Str]
+export { };
 
-interface String {
-  splits: <S extends string, D extends string>(delimiter: D) => Splitted<S, D>
+declare global {
+  type Splitted<Str extends string, Delimiter extends string> = string extends Str ? string[] :
+    Str extends '' ? [] :
+    Str extends `${infer Chunk}${Delimiter}${infer Rest}`
+    ? [Chunk, ...Splitted<Rest, Delimiter>]
+    : [Str]
+  export interface String {
+    splits: <S extends string, D extends string>(delimiter: D) => Splitted<S, D>
+  }
 }
